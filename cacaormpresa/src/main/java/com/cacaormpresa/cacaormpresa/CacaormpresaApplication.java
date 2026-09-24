00e6ca53ddfed9c9217f41f70f174759a8f61fcb
RoymasE <u23324789@utp.edu.pe>
2026-09-24 09:19:17 -0500

    1

---
+++ cacaormpresa/src/main/java/com/cacaormpresa/cacaormpresa/CacaormpresaApplication.java
@@ -0,0 +1,13 @@
+package com.cacaormpresa.cacaormpresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacaormpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacaormpresaApplication.class, args);
+	}
+
+}
---
+++ cacaormpresa/src/test/java/com/cacaormpresa/cacaormpresa/CacaormpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cacaormpresa.cacaormpresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacaormpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
